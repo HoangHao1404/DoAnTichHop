f995971c2a1d7b0e1a4378f1f3034161050e73f8
Duy Tan <tranduytan4@dtu.edu.vn>
2025-11-18 23:33:32 +0700

    Cau truc du an

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="p-10 bg-gray-900 text-center">
+      <h1 className="text-4xl font-bold text-blue-500 mt-20">
+        Tailwind ĐANG HOẠT ĐỘNG 😎🔥
+      </h1>
+    </div>
+  );
+}
+
+export default App;
