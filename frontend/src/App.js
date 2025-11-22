5098592b055ce5c750d261b98d499b8f6f10be60
HoangHao <hao14042004@gmail.com>
2025-11-22 14:31:37 +0700

    init haohao branch

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,10 @@
 import "./App.css";
+import Dashboard from "./pages/Dashboard";
 
 function App() {
   return (
-    <div className="p-10 bg-gray-900 text-center">
-      <h1 className="text-4xl font-bold text-blue-500 mt-20">
-        Tailwind ĐANG HOẠT ĐỘNG 😎🔥
-      </h1>
+    <div>   
+        <Dashboard />
     </div>
   );
 }
