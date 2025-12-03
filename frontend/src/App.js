41b7c6e7628eac830272326fcd0fd9551189722b
unknown <nguyenvu642004@gmail.com>
2025-12-03 18:33:28 +0700

    fix: merge branch Tun

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import "./App.css";
 import Dashboard from "./pages/Dashboard"
+import MyReport from "./pages/MyReports"
 // import Report from "./components/Report"
 function App() {
   return (
     <div>
-      <Dashboard/>
+        <Dashboard/>  
       {/* <Report/> */}
+      <MyReport/>
     </div>
   );
 }
