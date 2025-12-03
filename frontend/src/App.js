74560b4db656297ec7ace19f7098a996fc860ad4
quocvo114 <quocvovan123@gmail.com>
2025-12-03 14:29:06 +0700

    update: Dashboard

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,15 @@
 import "./App.css";
 import Dashboard from "./pages/Dashboard"
 // import Report from "./components/Report"
+// import SignIn from "./pages/SignIn"
+// import Register from "./pages/Register"
 function App() {
   return (
     <div>
       <Dashboard/>
       {/* <Report/> */}
+      {/* <SignIn/> */}
+      {/* <Register/> */}
     </div>
   );
 }
