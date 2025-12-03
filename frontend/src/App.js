80939a76334c23192824f0c51a1a167d36552baa
Duy Tan <tranduytan4@dtu.edu.vn>
2025-12-03 22:28:24 +0700

    Test giao dieN

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,13 +3,14 @@ import "./App.css";
 // import Dashboard from "./pages/Dashboard"
 import MyReports from "./pages/MyReports";
 // import Report from "./components/Report"
+import SignIn from "./pages/SignIn";
 function App() {
   return (
     <div>
       {/*<Dashboard/>*/}
       {/*<Report/>*/}
-      <MyReports/>
-
+      {/* <MyReports/> */}
+      <SignIn />
     </div>
   );
 }
