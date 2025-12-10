e1880f30c4cedf0292c4fa173b0960e4ad0c0f2b
Huey Luu <tsukiji@example.com>
2025-12-10 21:44:00 +0700

    trang public

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,8 @@
 import "./App.css";
+import Public_page from "./pages/Public_page";
 
 function App() {
-  return (
-    <div className="p-10 bg-gray-900 text-center">
-      <h1 className="text-4xl font-bold text-blue-500 mt-20">
-        Tailwind ĐANG HOẠT ĐỘNG 😎🔥
-      </h1>
-    </div>
-  );
+  return <Public_page />;
 }
 
 export default App;
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,7 +2,24 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: [
+          "Inter",
+          "ui-sans-serif",
+          "system-ui",
+          "-apple-system",
+          "Segoe UI",
+          "Roboto",
+          "Helvetica Neue",
+          "Arial",
+          "Noto Sans",
+          "Apple Color Emoji",
+          "Segoe UI Emoji",
+          "Segoe UI Symbol",
+        ],
+      },
+    },
   },
   plugins: [],
 };
