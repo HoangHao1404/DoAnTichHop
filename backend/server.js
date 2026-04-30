255704d0764a7d400f952d3d75a53867577d4ca4
Huey Luu <tsukiji@example.com>
2026-04-30 17:24:43 +0700

    fix loi connect backend

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,8 @@ dns.setDefaultResultOrder("ipv4first");
 dns.setServers(["8.8.8.8", "1.1.1.1"]);
 
 require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
 const connectDB = require("./src/config/database");
 const authRoutes = require("./src/services/auth/auth.routes");
 const userRoutes = require("./src/services/user/user.routes");
